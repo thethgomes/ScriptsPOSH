3ac0460a6c1ffaedc7792b5a7a2a1d390a967ec7
thethgomes <48591555+thethgomes@users.noreply.github.com>
2019-04-21 11:40:20 -0300

    Add files via upload

---
+++ consulta servico.ps1
@@ -0,0 +1,11 @@
+﻿# Consultar serviço do Windows
+Clear-Host
+$Serv = Get-Service -Name spooler
+If ($Serv.Status -eq "Running") 
+    {
+     Write-Host "Serviço Em execuçao"
+     }
+     Else
+     {
+     Write-Host "Serviço Parado"
+     }
